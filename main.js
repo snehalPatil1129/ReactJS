240a2ac95cb6ef5df58fe12ce373f765283a013c
Snehal Patil <snehal.patil@Eternus.local>
2018-02-22 16:58:43 +0530

    ReactApp

---
+++ main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+setTimeout(() => {
+   ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App.jsx';
+// // ReactDOM.render(<App headerProp = "Header from props..." contentProp = "Content
+// //    from props..."/>, document.getElementById('app'));
+
+// // export default App;
+
+
+// ReactDOM.render(<App />, document.getElementById('app'));
